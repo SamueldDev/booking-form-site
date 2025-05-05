69c36cfa48ee72fb022a167b9e040b796ae11019
samueldDev <samuelf8908>
2025-05-05 13:52:08 +0100

    added social iocns

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,35 @@
 
 
-import React from 'react'
-
+import React from 'react';
+import { Facebook, Twitter, Instagram } from 'lucide-react';
 
 export default function Footer() {
   return (
     <footer className="bg-blue-600 text-white py-6 mt-12">
-      <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
-        {/* Logo */}
+      <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-4">
+      
         <div className="text-xl font-bold">
           <a href="/" className="text-white">PeakMentor</a>
         </div>
 
         {/* Social Media Icons */}
         <div className="flex gap-6">
-          <a href="#" className="hover:text-blue-200">Facebook</a>
-          <a href="#" className="hover:text-blue-200">Twitter</a>
-          <a href="#" className="hover:text-blue-200">Instagram</a>
+          <a href="#" className="hover:text-blue-200" aria-label="Facebook">
+            <Facebook size={20} />
+          </a>
+          <a href="#" className="hover:text-blue-200" aria-label="Twitter">
+            <Twitter size={20} />
+          </a>
+          <a href="#" className="hover:text-blue-200" aria-label="Instagram">
+            <Instagram size={20} />
+          </a>
         </div>
 
-        <div className="text-sm">
+       
+        <div className="text-sm text-center md:text-right">
           &copy; 2025 PeakMentor. All rights reserved.
         </div>
       </div>
     </footer>
   );
 }
-
